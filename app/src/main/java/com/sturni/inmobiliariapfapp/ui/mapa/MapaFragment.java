6dda58d1cabb794236bf7e999d02a5fde5f73eba
IvanSturni <64703717+IvanSturni@users.noreply.github.com>
2021-04-26 21:14:26 -0300

    Implementación de mapa Removido botón flotante de la vista de menú

---
+++ app/src/main/java/com/sturni/inmobiliariapfapp/ui/mapa/MapaFragment.java
@@ -0,0 +1,67 @@
+package com.sturni.inmobiliariapfapp.ui.mapa;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.sturni.inmobiliariapfapp.R;
+
+public class MapaFragment extends Fragment {
+
+    private static final LatLng UBICACION = new LatLng(-33.3026, -66.3391);
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap map) {
+            map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            CameraPosition pos = new CameraPosition.Builder()
+                    .target(UBICACION)
+                    .zoom(17f)
+                    .build();
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+            map.addMarker(new MarkerOptions()
+                    .position(UBICACION)
+                    .title("Inmobiliaria SturniBossa"));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mapa, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}

8c401618d48a2f6048a36e3fe6c3e869f67514b8
IvanSturni <64703717+IvanSturni@users.noreply.github.com>
2021-04-27 00:41:47 -0300

    Ver inmuebles, con botón para (TODO) ver detalles

---
+++ app/src/main/java/com/sturni/inmobiliariapfapp/ui/inmuebles/InmuebleAdapter.java
@@ -0,0 +1,74 @@
+package com.sturni.inmobiliariapfapp.ui.inmuebles;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.sturni.inmobiliariapfapp.R;
+import com.sturni.inmobiliariapfapp.modelo.Inmueble;
+
+import java.util.ArrayList;
+
+public class InmuebleAdapter extends RecyclerView.Adapter<InmuebleAdapter.InmuebleViewHolder> {
+    private ArrayList<Inmueble> lista;
+    private Context context;
+
+    public InmuebleAdapter(Context context, ArrayList<Inmueble> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public InmuebleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.fila_inmueble, parent, false);
+        return new InmuebleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InmuebleViewHolder holder, int position) {
+        holder.tvDireccion.setText(lista.get(position).getDireccion());
+        holder.tvPrecio.setText("$"+lista.get(position).getPrecio());
+        String imgsrc = lista.get(position).getImagen();
+        Glide.with(context)
+                .load(lista.get(position).getImagen())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.ivFoto);
+
+        holder.ibDetalles.setOnClickListener(v -> {
+            // TODO: IMPLEMENTAR DETALLES DE INMUEBLE
+            Log.d("TODO", "onClick: Ver detalles de inmueble con id " + lista.get(position).getIdInmueble());
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+
+    class InmuebleViewHolder extends RecyclerView.ViewHolder {
+        TextView tvDireccion, tvPrecio;
+        ImageView ivFoto;
+        ImageButton ibDetalles;
+
+        public InmuebleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvDireccion = itemView.findViewById(R.id.tvDireccion);
+            tvPrecio = itemView.findViewById(R.id.tvPrecio);
+            ivFoto = itemView.findViewById(R.id.ivFoto);
+            ibDetalles = itemView.findViewById(R.id.ibDetalles);
+        }
+    }
+}

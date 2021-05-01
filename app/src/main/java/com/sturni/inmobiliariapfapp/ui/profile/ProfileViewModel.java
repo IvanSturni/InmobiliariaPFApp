f415f8050c875042587d41573246e4bc4dfe209c
MatiasBossa <matubossa@gmail.com>
2021-05-01 10:39:11 -0300

    profile fragment

--- app/src/main/java/com/sturni/inmobiliariapfapp/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/sturni/inmobiliariapfapp/ui/profile/ProfileViewModel.java
@@ -10,6 +10,7 @@ import com.sturni.inmobiliariapfapp.request.ApiClient;
 public class ProfileViewModel extends ViewModel {
 
     private MutableLiveData<Propietario> propetarioMutable;
+    private MutableLiveData<String> buttonEditMutable;
 
     public LiveData<Propietario> getPropietarioMutable(){
         if(propetarioMutable == null){
@@ -18,9 +19,32 @@ public class ProfileViewModel extends ViewModel {
         return propetarioMutable;
     }
 
+    public LiveData<String>  getButtonEditMutable(){
+        if(buttonEditMutable == null){
+            buttonEditMutable = new MutableLiveData<>();
+        }
+        return buttonEditMutable;
+    }
+
+
     public void loadProfile(){
         ApiClient api = ApiClient.getApi();
         Propietario propietario = api.obtenerUsuarioActual();
         propetarioMutable.setValue(propietario);
+    }
+
+    public void saveProfile(Propietario prop){
+        ApiClient api = ApiClient.getApi();
+        api.actualizarPerfil(prop);
+        propetarioMutable.setValue(prop);
+    }
+
+    public void toggleButton(Propietario prop){
+        if(buttonEditMutable.getValue() == "Editar"){
+            buttonEditMutable.setValue("Guardar");
+        }else{
+            buttonEditMutable.setValue("Editar");
+            saveProfile(prop);
+        }
     }
 }
--- app/src/main/java/com/sturni/inmobiliariapfapp/ui/profile/profile.java
+++ app/src/main/java/com/sturni/inmobiliariapfapp/ui/profile/profile.java
@@ -8,15 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.sturni.inmobiliariapfapp.R;
 import com.sturni.inmobiliariapfapp.modelo.Propietario;
+import com.sturni.inmobiliariapfapp.request.ApiClient;
 
 public class profile extends Fragment {
 
     private ProfileViewModel profileViewModel;
     private EditText dni_et, name_et, lastname_et, email_et, phone_et, password_et, code_et;
+    private Button editButton;
 
     public static profile newInstance() {
         return new profile();
@@ -25,7 +28,7 @@ public class profile extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_perfil, container, false);
+        View root = inflater.inflate(R.layout.profile_fragment, container, false);
 
         initComponents(root);
 
@@ -41,10 +44,16 @@ public class profile extends Fragment {
             }
         });
 
+        profileViewModel.getButtonEditMutable().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                toggleEditForm();
+            }
+        });
+
         profileViewModel.loadProfile();
 
         return root;
-
 
     }
 
@@ -56,7 +65,32 @@ public class profile extends Fragment {
         email_et = root.findViewById(R.id.email_et);
         phone_et = root.findViewById(R.id.phone_et);
         password_et = root.findViewById(R.id.password_et);
+        editButton = root.findViewById(R.id.editButton);
 
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Propietario prop = new Propietario();
+
+                prop.setDni(Long.parseLong(dni_et.getText().toString()));
+                prop.setNombre(name_et.getText().toString());
+                prop.setApellido(lastname_et.getText().toString());
+                prop.setEmail(email_et.getText().toString());
+                prop.setTelefono(phone_et.getText().toString());
+                prop.setContraseña(password_et.getText().toString());
+
+                profileViewModel.toggleButton(prop);
+            }
+        });
+
+    }
+
+    public void toggleEditForm(){
+        dni_et.setEnabled(!dni_et.isEnabled());
+        name_et.setEnabled(!name_et.isEnabled());
+        lastname_et.setEnabled(!lastname_et.isEnabled());
+        email_et.setEnabled(!email_et.isEnabled());
+        phone_et.setEnabled(!phone_et.isEnabled());
     }
 
 }
